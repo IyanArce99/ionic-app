7771478a5a415a5717ad7d16015f19f45c5eb762
javi95z <javi.95.zgz@gmail.com>
2024-01-29 11:08:26 +0100

    App development

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,19 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  }
+    loadChildren: () =>
+      import('./tabs/tabs.module').then((m) => m.TabsPageModule),
+  },
+  {
+    path: 'detail/:id',
+    loadChildren: () =>
+      import('./detail/detail.module').then((m) => m.DetailPageModule),
+  },
 ];
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
--- src/app/tab2/tab2.module.ts
+++ src/app/tab2/tab2.module.ts
@@ -1,20 +1,13 @@
-import { IonicModule } from '@ionic/angular';
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { Tab2Page } from './tab2.page';
-import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
-
+import { IonicModule } from '@ionic/angular';
+import { ShowItemComponent } from '../show-item/show-item.component';
 import { Tab2PageRoutingModule } from './tab2-routing.module';
+import { Tab2Page } from './tab2.page';
 
 @NgModule({
-  imports: [
-    IonicModule,
-    CommonModule,
-    FormsModule,
-    ExploreContainerComponentModule,
-    Tab2PageRoutingModule
-  ],
-  declarations: [Tab2Page]
+  imports: [IonicModule, CommonModule, FormsModule, Tab2PageRoutingModule],
+  declarations: [Tab2Page, ShowItemComponent],
 })
 export class Tab2PageModule {}
